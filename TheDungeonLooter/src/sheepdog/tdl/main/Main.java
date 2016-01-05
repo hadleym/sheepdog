e16142a982219d2d5ffd94336bb58ed3de0bc5ef
Hadley <hadleym@iMac-9.local>
2016-01-04 20:38:10 -0700

    episode 12

--- TheDungeonLooter/src/sheepdog/tdl/main/Main.java
+++ TheDungeonLooter/src/sheepdog/tdl/main/Main.java
@@ -1,12 +1,21 @@
 package sheepdog.tdl.main;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import sheepdog.gop.main.GameWindow;
 import sheepdog.gop.main.SpriteSheet;
+import sheepdog.tdl.gameloop.GameLoop;
 public class Main {
 
+	public static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	public static int width= gd.getDisplayMode().getWidth();
+	public static int height = gd.getDisplayMode().getHeight();
+	
 	static SpriteSheet blocks = new SpriteSheet();
 	public static void main(String[] args) {
-		GameWindow frame = new GameWindow("TheDLooter", 1280, 720);
+		GameWindow frame = new GameWindow("TheDLooter", width, height);
 		frame.setFullscreen(1);
+		frame.add(new GameLoop(width, height));
 		frame.setVisible(true);
 		
 		
